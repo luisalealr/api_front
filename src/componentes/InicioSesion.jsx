d21b427e99fee2f3bdab4dc35d341cfdd7c6fde5
luisalealr <luisafernandalr@ufps.edu.co>
2024-07-23 16:45:15 -0500

    Inicindo el login

--- src/componentes/InicioSesion.jsx
+++ src/componentes/InicioSesion.jsx
@@ -3,9 +3,21 @@ const InicioSesion = () => {
 
     return(
         <>
-        <div className="bg-[#D0F25E] max-w-fit max-h-fit">
+        <div className="h-[100vh] w-[100%] flex flex-row">
+            <div className="bg-[#D0F25E] h-[100vh] w-[30%] flex flex-col justify-center items-center">
+                <div className="w-[90%] mb-16">
+                    <h3>Droguería</h3>
+                    <h1>La Nueva Esperanza</h1>
+                </div>
+                <div className="w-[90%] flex flex-col justify-center items-center">
+                    <img className="rounded-full h-80 w-auto" src="../../public/img/logo.png" alt="" />
+                </div>
+            </div>
+            <div className="bg-white h-[100vh] w-[70%] ">
 
+            </div>
         </div>
+
         </>
     );
 };
