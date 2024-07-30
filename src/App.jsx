5bb1a4a5a112814a0c2a0a5cfa66d2ef0fa1dd5d
luisalealr <luisafernandalr@ufps.edu.co>
2024-07-30 17:13:25 -0500

    Rutas denuevo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SideBar from './componentes/admin/templates/Sidebar';
 import SideBarVendedor from './componentes/vendedor/templates/SidebarVendedor';
 import InicioSesion from './componentes/InicioSesion';
