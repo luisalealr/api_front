4828a6aa7ae5727a25bdcfa8441d2be5019e67e2
luisalealr <luisafernandalr@ufps.edu.co>
2024-07-31 16:33:50 -0500

    Filtrar productos ascendente y descendente funcional

--- src/componentes/admin/productos/ListarProductos.jsx
+++ src/componentes/admin/productos/ListarProductos.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import TemplateAdmin from "../templates/TemplateAdmin";
+import { HiFilter } from "react-icons/hi";
 import { IoSearch } from "react-icons/io5";
 import { useNavigate, useLocation } from "react-router-dom";
 import { toast, ToastContainer } from 'react-toastify';
 import axios from 'axios';
 import { API_URL } from "../../../config"; // Ajusta la ruta según tu estructura de archivos
 import 'react-toastify/dist/ReactToastify.css';
-import { getProductosAsc, getProductosDesc } from "../../../services/ProductService";
+import { getProductosAsc, getProductosDesc, getProducts } from "../../../services/ProductService";
 import { getAllProducts } from "../../../services/ProductService";
 import TablaProductos from "./TablaProductos";
 
 const ListarProductos = () => {
   const [productos, setProductos] = useState([]);
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
   const [productosNoActivos, setProductosNoActivos] = useState([]);
   const [productosOrdenados, setProductosOrdenados] = useState([]);
   const [buscarDesc, setBuscarDec] = useState('');
@@ -19,9 +21,32 @@ const ListarProductos = () => {
   const location = useLocation();
   const message = location.state?.message;
   let results = [];
+  const [selectedRadio, setSelectedRadio] = useState('');
+
+  const handleRadioChange = (event) => {
+    setSelectedRadio(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setIsFilterVisible(!isFilterVisible);
+    if(selectedRadio == 'option1'){
+      listarAscendente();
+    }else if(selectedRadio == 'option2'){
+      listarDescendente();
+    }else if(selectedRadio == 'defecto'){
+      obtenerProductos();
+    }
+    setSelectedRadio('');
+  };
 
   const crearProducto = () => {
     navigate('/registrar_producto');
+  };
+
+  const obtenerProductos = async () =>{
+    const productosActivos = await getProducts();
+    setProductos(productosActivos);
+    setProductosOrdenados(productosActivos); 
   };
 
   useEffect(() => {
@@ -95,6 +120,10 @@ const ListarProductos = () => {
     setProductosOrdenados(productosDescendente);
   };
 
+  const filtrar = () => {
+    setIsFilterVisible(!isFilterVisible);
+  };
+
   return (
     <TemplateAdmin>
       <ToastContainer />
@@ -102,59 +131,104 @@ const ListarProductos = () => {
         <div className="bg-[#D0F25E]">
           <h2 className="py-2 px-6 font-semibold text-xl">Productos</h2>
         </div>
-        <div className="w-full h-16 flex items-center justify-between">
-          <div className="w-[30%] shadow-sm border border-[#999999] rounded-md flex flex-row items-center ml-6">
-            <IoSearch color="rgba(141, 182, 0, 0.79)" fontSize={25} className="m-1" />
-            <input
-              value={buscarDesc}
-              onChange={buscador}
-              type="text"
-              placeholder="Buscar producto"
-              className="w-full text-sm h-8 border-none rounded-md"
-            />
+        <div className="w-full  py-3 flex items-center justify-between">
+          <div className="w-[60%] flex items-center">
+            <div className="flex flex-col ">
+              <div  onClick={filtrar} className="w-fit shadow-sm flex flex-row px-2 mx-9 py-1 border border-[#999999] justify-center items-center rounded-md cursor-pointer">
+                <HiFilter color="rgba(141, 182, 0, 0.79)" fontSize={25}/>
+                <h5>Filtro</h5>
+              </div>
+              {isFilterVisible && (
+                <div className="border border-[#999999] absolute left-80 top-24 mt-2 w-auto flex flex-col text-sm bg-gray-200 py-2 px-6 rounded-md shadow-xl">
+                  <div className="flex flex-col mb-3">
+                    <h5>Ordenar por cantidad</h5>
+                  </div>
+                  <div className="flex flex-col mb-1">
+                    <label>
+                      <input 
+                        type="radio" 
+                        name="defecto" 
+                        value="defecto"
+                        checked={selectedRadio === 'defecto'}
+                        onChange={handleRadioChange}
+                        className="mr-2"/>
+                        Por defecto
+                    </label>
+                    <label>
+                      <input 
+                        type="radio" 
+                        name="option1" 
+                        value="option1"
+                        checked={selectedRadio === 'option1'}
+                        onChange={handleRadioChange}
+                        className="mr-2"/>
+                        Ascendente
+                    </label>
+                    <label>
+                      <input 
+                        type="radio" 
+                        name="option2" 
+                        value="option2" 
+                        checked={selectedRadio === 'option2'}
+                        onChange={handleRadioChange}
+                        className="mr-2"/>
+                        Desscendente
+                    </label>
+                  </div>
+                  <button 
+                    onClick={handleSubmit}
+                    className="w-fit shadow-sm rounded-md py-[2px] border mt-2 border-[#999999] bg-white px-2"
+                  >Aceptar</button>
+                </div>
+              )}
+            </div>
+            <div className="w-[50%] shadow-sm border border-[#999999] rounded-md flex flex-row items-center ">
+              <IoSearch color="rgba(141, 182, 0, 0.79)" fontSize={25} className="m-1" />
+              <input
+                value={buscarDesc}
+                onChange={buscador}
+                type="text"
+                placeholder="Buscar producto"
+                className="w-full text-sm h-8 border-none rounded-md"
+              />
+            </div>
           </div>
-          <button onClick={listarAscendente}>Ascendente</button>
-          <button onClick={listarDescendente}>Descendente</button>
-          <button onClick={crearProducto} className="mr-6 py-1 rounded-md px-6 shadow hover:bg-[#b0d144] bg-[#8DB600]">
+          <button onClick={crearProducto} className="mr-6 py-1 rounded-md w-fit px-6 shadow hover:bg-[#b0d144] bg-[#8DB600]">
             Registrar Producto
           </button>
         </div>
-        <div className="bg-[#D0F25E] h-6 w-full"></div>
-        <div className="flex-grow overflow-hidden relative">
-          <div className="w-full h-full overflow-x-auto absolute bottom-0">
-            <table className="text-sm min-w-full">
-              <thead className="bg-[#95A09D] text-left">
-                <tr>
-                  <th className="pl-5">N° del producto</th>
-                  <th>Nombre</th>
-                  <th>Categoría</th>
-                  <th>Proveedor</th>
-                  <th>Peso</th>
-                  <th className="pl-5">Precio</th>
-                  <th>Cantidad en Stock</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody className="">
-                {results.map((producto, index) => (
-                  <TablaProductos
-                    key={index}
-                    productoId={producto.id_producto}
-                    nombre={producto.nombre}
-                    categoria={producto.categoria.descripcion}
-                    proveedor={producto.proveedor.nombre}
-                    peso={producto.peso}
-                    precio={producto.precio_unitario}
-                    cantidad={producto.cantidad}
-                    active={producto.isActive}
-                    onDisable={handleDisable}
-                    onEnable={handleEnable}
-                  />
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <div className="bg-[#D0F25E] py-3 w-full"></div>
+        <table className=" text-sm">
+          <thead className="bg-[#95A09D] text-left">
+            <tr>
+              <th className="pl-5">N° del producto</th>
+              <th>Nombre</th>
+              <th>Categoría</th>
+              <th>Proveedor</th>
+              <th>Peso</th>
+              <th className="pl-5">Precio</th>
+              <th>Cantidad en Stock</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((producto, index) => (
+              <TablaProductos
+                key={index}
+                productoId={producto.id_producto}
+                nombre={producto.nombre}
+                categoria={producto.categoria.descripcion}
+                proveedor={producto.proveedor.nombre}
+                peso={producto.peso}
+                precio={producto.precio_unitario}
+                cantidad={producto.cantidad}
+                active={producto.isActive}
+                onDisable={handleDisable}
+                onEnable={handleEnable}
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
     </TemplateAdmin>
   );
